fc5a20be1da565496e02b650cb2c000b6ec6d1f0
Abdurrahman <abduqah@gmail.com>
2020-10-08 21:00:32 +0200

    Modified db, rename table in DB

--- lib/generators/shorten_url/templates/migration.rb
+++ lib/generators/shorten_url/templates/migration.rb
@@ -1,13 +1,13 @@
 class CreateShortUrls < ActiveRecord::Migration[5.2]
   def change
-    create_table :short_urls do |t|
+    create_table :shorten_url_short_urls do |t|
       t.string :url, null: false
       t.string :short_url, unique: true
 
       t.timestamps
     end
     
-    add_index :short_urls, :url
-    add_index :short_urls, :short_url, unique: true
+    add_index :shorten_url_short_urls, :url
+    add_index :shorten_url_short_urls, :short_url, unique: true
   end
 end
