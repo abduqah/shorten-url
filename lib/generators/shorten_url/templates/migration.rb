c10d2ac058ae2dc50569288859798567d95cc3b7
Abdurrahman <abduqah@gmail.com>
2020-10-08 20:41:33 +0200

    Modified migration, naming change

--- lib/generators/shorten_url/templates/migration.rb
+++ lib/generators/shorten_url/templates/migration.rb
@@ -7,7 +7,7 @@ class CreateShortUrls < ActiveRecord::Migration[5.2]
       t.timestamps
     end
     
-    add_index :short_urls, :full_link
+    add_index :short_urls, :url
     add_index :short_urls, :short_url, unique: true
   end
 end
